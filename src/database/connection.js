712aeeabfe0ebfca6df9a2eab2ce27404f7d993a
FredBettecher <fredbettecher@gmail.com>
2023-03-21 21:52:57 -0300

    feat: create connection with database

---
+++ src/database/connection.js
@@ -0,0 +1,18 @@
+import pkg from "pg";
+import dotenv from "dotenv";
+dotenv.config();
+
+const { Pool } = pkg;
+
+const configDatabase = {
+    connectionString: process.env.DATABASE_URL,
+    ...(process.env.NODE_ENV === "production" && {
+      ssl: {
+        rejectUnauthorized: false,
+        },
+    }),
+};
+  
+  const connection = new Pool(configDatabase);
+  
+  export default connection;
